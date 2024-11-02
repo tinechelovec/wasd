302618647518b8e0fed93c89934415651be90928
Каурдаков Кирилл <kirilkaurd2@stud.vvsu.ru>
2024-11-02 14:08:30 +1000

    Добавьте файлы проекта.

---
+++ да/Windows2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace да
+{
+    /// <summary>
+    /// Логика взаимодействия для Windows2.xaml
+    /// </summary>
+    public partial class Windows2 : Window
+    {
+        public Windows2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
